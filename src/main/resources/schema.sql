b51341f9a9132e4da2932adb0a416b1d49e0a469
c21081270 <LiuF29@cardiff.ac.uk>
2022-11-22 22:49:09 +0000

    Added new table repair_Booking to store the appointment

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,4 +15,20 @@ CREATE TABLE IF NOT EXISTS repair_Cafe
     address         varchar(2000) not null,
     primary key (`cafe_id`)
     )
+
+-- -----------------------------------------------------
+-- Table `repair_Booking`
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS repair_Booking;
+
+
+CREATE TABLE IF NOT EXISTS repair_Booking
+(
+    booking_id      varchar(15)  not null,
+    firstName       varchar(200) not null,
+    lastName        varchar(200) not null,
+    email           varchar(200) not null
+    primary key (`booking_id`)
+    )
     engine = InnoDB;
