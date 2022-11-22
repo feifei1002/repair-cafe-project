1a049bff1ad7fe91b6be4bb75376b2b287a01dae
c21079342 <pollyl@cardiff.ac.uk>
2022-11-22 12:15:31 +0000

    Inital commit

---
+++ src/main/java/uk/cf/ac/nccteam11/NcCteam11Application.java
@@ -0,0 +1,13 @@
+package uk.cf.ac.nccteam11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcCteam11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcCteam11Application.class, args);
+    }
+
+}
---
+++ src/test/java/uk/cf/ac/nccteam11/NcCteam11ApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.cf.ac.nccteam11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcCteam11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
