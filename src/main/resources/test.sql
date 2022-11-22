7c443e0246d3585f0a11a0de8ebf24460f32e425
c21081270 <LiuF29@cardiff.ac.uk>
2022-11-22 21:03:36 +0000

    Created test.sql and inserted some data into database for testing purpose

---
+++ src/main/resources/test.sql
@@ -0,0 +1,29 @@
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('1', 'Abergavenny', 'Abergavenny Community Centre, The Old School, Park Street, Abergavenny NP7 5YB');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('2', 'Bangor', 'SBangor Students'' Union, Room 3, Activities Hub, Ffriddoedd Site, Ffriddoedd Road, Bangor LL57 2JW');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('3', 'Barford', 'Scout Hut, Wasperton Lane, Barford, Warwick CV35 8DT');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('4', 'Beaumaris', 'Canolfan Hamdden Beaumaris, Rating Row, Beaumaris, Ynys Mon LL58 8AL');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('5', 'Boat of Garten', 'Boat of Garten Community Hall, Reidhaven Park, Craigie Avenue Boat of Garten, Highland PH24 3BL');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('6', 'Bridgend', 'Bryncethin Community Centre, Ogmore Terrace, Bryncethin, Bridgend CF32 9YF');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('7', 'Bridgend College Pop Up Shop', '22 The Rhiw Bridgend CF31 3BL United Kingdom');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('8', 'Caerau', 'Caerau Menshed, Bangor Terrace, Nantyffyllon, Bridgend CF34 0HU');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('9', 'Caerphilly', 'Caerphilly Miners Centre For The Community, Watford Road, Caerphilly CF83 1ET');
+
+INSERT INTO repair_Cafe(cafe_id, name, address)
+VALUES ('10', 'Caerphilly (The Furniture Revival)', 'The Furniture Revival, Unit 21 Lawn Industrial Estate, Rhymey, Tredegar, Gwent NP22 5PW');
