17a99079a6cf0aed1bbe3f49db59eff4e9866137
c21081270 <LiuF29@cardiff.ac.uk>
2022-12-01 14:46:26 +0000

    implemented get method into the user controller

--- src/main/java/uk/cf/ac/nccteam11/account/web/UserController.java
+++ src/main/java/uk/cf/ac/nccteam11/account/web/UserController.java
@@ -1,4 +1,23 @@
 package uk.cf.ac.nccteam11.account.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import uk.cf.ac.nccteam11.account.service.UserService;
+
+@Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService usrService){
+        this.userService = usrService;
+    }
+
+    @GetMapping("user/sign-up")
+    public ModelAndView getNewSignUpForm(Model model){
+        model.addAttribute("signUpForm", new SignUpForm());
+        var mv = new ModelAndView("sign-up", model.asMap());
+        return mv;
+    }
 }
