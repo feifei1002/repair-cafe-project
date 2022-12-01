b312f0e484a26c128eea2894ab24201cee7d4c51
c21081270 <LiuF29@cardiff.ac.uk>
2022-12-01 14:37:06 +0000

    set up the basic html page and java class for the sign up form

---
+++ src/main/java/uk/cf/ac/nccteam11/account/web/SignUpForm.java
@@ -0,0 +1,20 @@
+package uk.cf.ac.nccteam11.account.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpForm {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String firstLineAddress;
+    private String town;
+    private String city;
+    private String postcode;
+
+}
