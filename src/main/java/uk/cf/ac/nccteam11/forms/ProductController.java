6ce326a90b17a31ce785e25e158a842c45f27c19
c21079342 <pollyl@cardiff.ac.uk>
2022-12-01 14:29:19 +0000

    made it possible to query through the products

--- src/main/java/uk/cf/ac/nccteam11/forms/ProductController.java
+++ src/main/java/uk/cf/ac/nccteam11/forms/ProductController.java
@@ -4,7 +4,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import uk.cf.ac.nccteam11.ProductService;
+import uk.cf.ac.nccteam11.service.ProductService;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/uk/cf/ac/nccteam11/service/ProductService.java
+++ src/main/java/uk/cf/ac/nccteam11/service/ProductService.java
@@ -1,4 +1,4 @@
-package uk.cf.ac.nccteam11;
+package uk.cf.ac.nccteam11.service;
 
 import uk.cf.ac.nccteam11.forms.ProductListRequest;
 import uk.cf.ac.nccteam11.forms.ProductListResponse;
