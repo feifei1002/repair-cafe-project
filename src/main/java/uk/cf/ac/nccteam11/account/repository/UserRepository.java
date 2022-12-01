22e8b7a5b168863c46202056220bc852b5782b81
c21081270 <LiuF29@cardiff.ac.uk>
2022-12-01 14:12:40 +0000

    implemented the user repository

--- src/main/java/uk/cf/ac/nccteam11/account/repository/UserRepository.java
+++ src/main/java/uk/cf/ac/nccteam11/account/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package uk.cf.ac.nccteam11.account.repository;
 
+import uk.cf.ac.nccteam11.account.domain.User;
+
+import java.util.List;
+
 public interface UserRepository {
+    List<User> getUsers();
+
+    void addUser(User newUser);
 }
