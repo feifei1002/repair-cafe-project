4c734ca06f096b331a407760f4554a3ed8e125d3
c21081270 <LiuF29@cardiff.ac.uk>
2022-12-02 01:04:59 +0000

    added error handling and validation (incomplete)

--- src/main/java/uk/cf/ac/nccteam11/account/web/SignUpForm.java
+++ src/main/java/uk/cf/ac/nccteam11/account/web/SignUpForm.java
@@ -4,18 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpForm {
     private Integer user_id;
+
+    @NotEmpty
     private String firstName;
+
+    @NotEmpty
     private String lastName;
+
+    @NotEmpty
+    @Pattern(regexp=".+@.+\\.[a-z]+", message = "Invalid characters")
     private String email;
+
+    @NotEmpty
+    @Min(value = 8, message = "Password must have minimum 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]$",
+    message = "Password must have minimum 1 upper case letter, 1 lowercase letter, 1 number and 1 special character")
     private String password;
+
+    @NotEmpty
     private String firstLineAddress;
+
+    @NotEmpty
     private String town;
+
+    @NotEmpty
     private String city;
+
+    @NotEmpty
     private String postcode;
 
 }
--- src/main/java/uk/cf/ac/nccteam11/account/web/UserController.java
+++ src/main/java/uk/cf/ac/nccteam11/account/web/UserController.java
@@ -1,8 +1,8 @@
 package uk.cf.ac.nccteam11.account.web;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,6 +10,8 @@ import uk.cf.ac.nccteam11.account.service.UserDTO;
 import uk.cf.ac.nccteam11.account.service.UserService;
 import uk.cf.ac.nccteam11.account.service.message.SaveUserRequest;
 import uk.cf.ac.nccteam11.account.service.message.SaveUserResponse;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -27,14 +29,18 @@ public class UserController {
     }
 
     @PostMapping("user/sign-up/add")
-    public ResponseEntity addNewUser(SignUpForm newSignUp, Model model){
-        UserDTO userDTO = new UserDTO(newSignUp.getUser_id(), newSignUp.getFirstName(), newSignUp.getLastName(),
-                                    newSignUp.getEmail(), newSignUp.getPassword(), newSignUp.getFirstLineAddress(),
-                                    newSignUp.getTown(), newSignUp.getCity(), newSignUp.getPostcode());
-        SaveUserRequest saveUserRequest = SaveUserRequest.of().userDTO(userDTO).build();
-        SaveUserResponse saveUserResponse = userService.addNewUser(saveUserRequest);
-        return ResponseEntity.ok().body("Sign up sucessufully!");
-//        var mv = new ModelAndView("redirect:/");
-//        return mv;
+    public ModelAndView addNewUser(@Valid SignUpForm newSignUp, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            var mv = new ModelAndView("sign-up", model.asMap());
+            return mv;
+        } else {
+            UserDTO userDTO = new UserDTO(newSignUp.getUser_id(), newSignUp.getFirstName(), newSignUp.getLastName(),
+                    newSignUp.getEmail(), newSignUp.getPassword(), newSignUp.getFirstLineAddress(),
+                    newSignUp.getTown(), newSignUp.getCity(), newSignUp.getPostcode());
+            SaveUserRequest saveUserRequest = SaveUserRequest.of().userDTO(userDTO).build();
+            SaveUserResponse saveUserResponse = userService.addNewUser(saveUserRequest);
+            var mv = new ModelAndView("redirect:/");
+            return mv;
+        }
     }
 }
