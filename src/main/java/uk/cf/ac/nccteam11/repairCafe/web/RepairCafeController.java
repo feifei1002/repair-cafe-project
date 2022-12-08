ce774a190ac5d89254999f1f69faa52df287cd79
c21081270 <LiuF29@cardiff.ac.uk>
2022-12-08 11:23:00 +0000

    updated controller and added a HTML page

--- src/main/java/uk/cf/ac/nccteam11/repairCafe/web/RepairCafeController.java
+++ src/main/java/uk/cf/ac/nccteam11/repairCafe/web/RepairCafeController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import uk.cf.ac.nccteam11.repairCafe.service.RepairCafeDTO;
 import uk.cf.ac.nccteam11.repairCafe.service.RepairCafeService;
@@ -12,6 +13,7 @@ import uk.cf.ac.nccteam11.repairCafe.service.message.RepairCafeListRequest;
 import uk.cf.ac.nccteam11.repairCafe.service.message.RepairCafeListResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class RepairCafeController {
@@ -27,6 +29,27 @@ public class RepairCafeController {
         var repairCafeListResponse = repairCafeService.getRepairCafes(repairCafeListRequest);
         model.addAttribute("repairCafes", repairCafeListResponse.getRepairCafes());
         return new ModelAndView("index");
+    }
+
+    @GetMapping("repair-cafes-list")
+    public ModelAndView getRepairCafes(@RequestParam(name = "q", required = false) Optional<String> query, Model model) {
+
+        List<RepairCafeDTO> repairCafes;
+
+        RepairCafeListRequest repairCafeListRequest = RepairCafeListRequest
+                .of()
+                .searchTerm(query)
+                .build();
+
+
+        var repairCafeListResponse = repairCafeService.getRepairCafes(repairCafeListRequest);
+
+
+        model.addAttribute("repairCafes", repairCafeListResponse.getRepairCafes());
+
+        var mv = new ModelAndView("repair-cafe-list", model.asMap());
+        return mv;
+
     }
 
     @GetMapping("repair/cafes")
