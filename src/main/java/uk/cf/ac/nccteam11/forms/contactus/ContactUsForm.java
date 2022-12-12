edb2dec0b956b134145ca3dc83fd82f260006efe
c21079342 <pollyl@cardiff.ac.uk>
2022-12-12 16:49:38 +0000

    added lombok and dtos for contact us

--- src/main/java/uk/cf/ac/nccteam11/forms/contactus/ContactUsForm.java
+++ src/main/java/uk/cf/ac/nccteam11/forms/contactus/ContactUsForm.java
@@ -1,9 +1,29 @@
 package uk.cf.ac.nccteam11.forms.contactus;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContactUsForm {
+
     private Integer instanceId;
+
+    @NotEmpty
     private String fullName;
+
+    @NotEmpty
+    @Pattern(regexp=".+@.+\\.[a-z]+", message = "Invalid characters")
     private String email;
+
+    @NotEmpty
     private String reason;
+
     private String message;
 }
