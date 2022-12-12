f0926276ca77923f85c77e514a942b5a3b9e974b
c21079342 <pollyl@cardiff.ac.uk>
2022-12-12 16:38:27 +0000

    added the contact us form data into the database

---
+++ src/main/java/uk/cf/ac/nccteam11/forms/contactus/ContactUsForm.java
@@ -0,0 +1,9 @@
+package uk.cf.ac.nccteam11.forms.contactus;
+
+public class ContactUsForm {
+    private Integer instanceId;
+    private String fullName;
+    private String email;
+    private String reason;
+    private String message;
+}
