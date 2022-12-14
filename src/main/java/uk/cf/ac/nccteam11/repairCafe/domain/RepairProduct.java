ad2e8941836aad5ea4d6b6bb7f536c44a499e9a5
c21081270 <LiuF29@cardiff.ac.uk>
2022-12-14 16:43:33 +0000

    fixed breaking problem caused by repair-borrow

--- src/main/java/uk/cf/ac/nccteam11/repairCafe/domain/RepairProduct.java
+++ src/main/java/uk/cf/ac/nccteam11/repairCafe/domain/RepairProduct.java
@@ -4,11 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
-import uk.cf.ac.nccteam11.repairCafe.service.RepairBorrowDTO;
-
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -29,29 +25,29 @@ public class RepairProduct {
     @Column("IS_APPROVED")
     private String isApproved;
 
-    @MappedCollection(idColumn = "PRODUCT_ID")
-//    private RepairBorrow repairBorrow;
-    private Set<RepairBorrow> repairBorrows;
-
-//    public static RepairProduct of(Integer productId, String productName, String condition, String brand, String status, String isApproved) {
-//        return new RepairProduct(productId, productName, condition, brand, status, isApproved, null);
+//    @MappedCollection(idColumn = "PRODUCT_ID")
+////    private RepairBorrow repairBorrow;
+//    private Set<RepairBorrow> repairBorrows;
+//
+////    public static RepairProduct of(Integer productId, String productName, String condition, String brand, String status, String isApproved) {
+////        return new RepairProduct(productId, productName, condition, brand, status, isApproved, null);
+////    }
+//    public void addBorrow(RepairBorrowDTO repairBorrowDTO){
+//        repairBorrows.add(RepairBorrow.of(repairBorrowDTO.getBorrowId(),
+//                repairBorrowDTO.getFirstName(), repairBorrowDTO.getLastName(),
+//                repairBorrowDTO.getEmail()));
 //    }
-    public void addBorrow(RepairBorrowDTO repairBorrowDTO){
-        repairBorrows.add(RepairBorrow.of(repairBorrowDTO.getBorrowId(),
-                repairBorrowDTO.getFirstName(), repairBorrowDTO.getLastName(),
-                repairBorrowDTO.getEmail()));
-    }
-
-    public void updateBorrow(RepairBorrowDTO repairBorrowDTO) {
-        RepairBorrow borrowToChange = getRepairBorrowByDTO(repairBorrowDTO);
-    }
-
-    private RepairBorrow getRepairBorrowByDTO(RepairBorrowDTO repairBorrowDTO) {
-        RepairBorrow borrowToChange = repairBorrows
-                .stream()
-                .filter(rb -> rb.getBorrowId().equals(repairBorrowDTO.getBorrowId()))
-                .findFirst()
-                .get();
-        return borrowToChange;
-    }
+//
+//    public void updateBorrow(RepairBorrowDTO repairBorrowDTO) {
+//        RepairBorrow borrowToChange = getRepairBorrowByDTO(repairBorrowDTO);
+//    }
+//
+//    private RepairBorrow getRepairBorrowByDTO(RepairBorrowDTO repairBorrowDTO) {
+//        RepairBorrow borrowToChange = repairBorrows
+//                .stream()
+//                .filter(rb -> rb.getBorrowId().equals(repairBorrowDTO.getBorrowId()))
+//                .findFirst()
+//                .get();
+//        return borrowToChange;
+//    }
 }
