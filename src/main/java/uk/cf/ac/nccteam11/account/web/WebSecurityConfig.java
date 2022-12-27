1aad8e387f5bc9128255ff591365b18edc680db4
c21081270 <LiuF29@cardiff.ac.uk>
2022-12-27 23:48:23 +0000

    authentication completed

--- src/main/java/uk/cf/ac/nccteam11/account/web/WebSecurityConfig.java
+++ src/main/java/uk/cf/ac/nccteam11/account/web/WebSecurityConfig.java
@@ -7,57 +7,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.DefaultLoginPageConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
-
-//package uk.cf.ac.nccteam11.account.web;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class WebSecurityConfig{
-//
-//    @Autowired
-//    private DataSource dataSource;
-//    AuthenticationManagerBuilder authenticationManagerBuilder;
-//
-//    @Autowired
-//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-//                .dataSource(dataSource)
-//                .usersByUsernameQuery("select email, password from user where email=?")
-//        ;
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().permitAll()
-//                .and()
-//                .logout().permitAll();
-//        return null;
-//    }
-//
-//    @Bean
-//    public PasswordEncoder encoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//}
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -69,17 +22,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .passwordEncoder(new BCryptPasswordEncoder())
-                .usersByUsernameQuery("select email, password from user where email = ?")
+                .usersByUsernameQuery("select email, password, enabled from user where email = ?")
                 .authoritiesByUsernameQuery("select email, role from user where email = ?");
 
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.removeConfigurer(DefaultLoginPageConfigurer.class);
+//        http.removeConfigurer(DefaultLoginPageConfigurer.class);
         http
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
-                                .mvcMatchers("/admin/**").permitAll()
+
+                                .mvcMatchers("/admin/**").authenticated()
+                                .mvcMatchers("/repair/booking/form").authenticated()
+                                .mvcMatchers("/repair/products-list").authenticated()
+                                .mvcMatchers("/repair-product/add").authenticated()
                                 .mvcMatchers("/css/**").permitAll()
                                 .mvcMatchers("/").permitAll()
                                 .mvcMatchers("/sign-up").permitAll()
